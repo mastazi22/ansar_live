0331e58a6d5fbc18a6428db290a744497a03fa50
arafat <shuvo1110101010@gmail.com>
2016-12-14 10:46:21 +0600

    update

--- local/app/modules/HRM/Views/Partial_view/not_verified_ansar_list.blade.php
+++ local/app/modules/HRM/Views/Partial_view/not_verified_ansar_list.blade.php
@@ -7,7 +7,9 @@
         <th>Birth Date</th>
         <th>Home District</th>
         <th>Thana</th>
-        <th>Action</th>
+        @if(UserPermission::userPermissionExists('entryverify'))
+            <th class="print-hide">Action</th>
+        @endif
 
     </tr>
     <tbody>
@@ -19,19 +21,26 @@
         <td>[[ansar.birth_date|dateformat:"DD-MMM-YYYY"]]</td>
         <td>[[ansar.unit]]</td>
         <td>[[ansar.thana]]</td>
-        <td>
-            <form action="{{URL::to('HRM/entryVerify/')}}" method="post" form-submit confirm-box="1" message="Are you want to verify this Ansar?" loading="loading[$index]" on-reset="loadPage()">
-                <input type="hidden" value="[[ansar.id]]" name="verified_id">
-                <button class="btn btn-primary btn-xs" title="verify" ng-disabled="loading[$index]">
-                    <i ng-hide="loading[$index]" class="fa fa-check"></i>
-                    <i ng-show="loading[$index]" class="fa fa-spinner fa-pulse"></i>
-                    &nbsp;Verify
-                </button>
-            </form>
-        </td>
+        @if(UserPermission::userPermissionExists('entryverify'))
+            <td class="print-hide">
+                <form action="{{URL::to('HRM/entryVerify/')}}" method="post" form-submit confirm-box="1"
+                      message="Are you want to verify this Ansar?" loading="loading[$index]" on-reset="loadPage()">
+                    <input type="hidden" value="[[ansar.id]]" name="verified_id">
+                    <button class="btn btn-primary btn-xs" title="verify" ng-disabled="loading[$index]">
+                        <i ng-hide="loading[$index]" class="fa fa-check"></i>
+                        <i ng-show="loading[$index]" class="fa fa-spinner fa-pulse"></i>
+                        &nbsp;Verify
+                    </button>
+                </form>
+            </td>
+        @endif
     </tr>
     <tr ng-if="data.ansars.length<=0">
-        <td class="warning" colspan="8">No Ansar Found</td>
+        @if(UserPermission::userPermissionExists('entryverify'))
+            <td class="warning" colspan="8">No Ansar Found</td>
+        @else
+            <td class="warning" colspan="8">No Ansar Found</td>
+        @endif
     </tr>
     </tbody>
 </table>
