b7ae54d60f2cc247ac653534c36a0c9af18445a1
arafat <shuvo1110101010@gmail.com>
2016-12-29 14:37:26 +0600

    update

--- local/app/Console/Kernel.php
+++ local/app/Console/Kernel.php
@@ -48,47 +48,47 @@ class Kernel extends ConsoleKernel
         $schedule->call(function () {
             Log::info("called : send_offer");
 //            //return;
-            $user = env('SSL_USER_ID','ansarapi');
-            $pass = env('SSL_PASSWORD','x83A7Z96');
-            $sid = env('SSL_SID','ANSARVDP');
-            $url = "http://sms.sslwireless.com/pushapi/dynamic/server.php";
-            $offered_ansar = OfferSMS::where('sms_try', 0)->where('sms_status', 'Queue')->take(10)->get();
-            foreach ($offered_ansar as $offer) {
-                DB::beginTransaction();
-                try {
-                    $a = $offer->ansar;
-//                    Log::info($a);
-//                    break;
-                    $dis = $offer->district->unit_name_eng;
-                    $body = 'You (ID:' . $offer->ansar_id . ') are offered for ' . $dis . ' as Rank ' . $a->designation->name_eng . ' Please type (ans YES/ans NO) and send to 6969 within 48 hours. Otherwise your offer will be cancelled - DC ' . strtoupper($dis);
-                    $phone = '88' . trim($a->mobile_no_self);
-                    $param = "user=$user&pass=$pass&sms[0][0]=$phone&sms[0][1]=" . urlencode($body) . "&sid=$sid";
-                    $crl = curl_init();
-                    curl_setopt($crl, CURLOPT_SSL_VERIFYPEER, FALSE);
-                    curl_setopt($crl, CURLOPT_SSL_VERIFYHOST, 2);
-                    curl_setopt($crl, CURLOPT_URL, $url);
-                    curl_setopt($crl, CURLOPT_HEADER, 0);
-                    curl_setopt($crl, CURLOPT_RETURNTRANSFER, 1);
-                    curl_setopt($crl, CURLOPT_POST, 1);
-                    curl_setopt($crl, CURLOPT_POSTFIELDS, $param);
-                    $response = curl_exec($crl);
-                    curl_close($crl);
-                    $r = Parser::xml($response);
-                    Log::info(json_encode($r));
-                    $offer->sms_try += 1;
-                    if (isset($r['PARAMETER']) && strcasecmp($r['PARAMETER'], 'OK') == 0) {
-                        $offer->sms_status = 'Send';
-                        $offer->save();
-                    } else {
-                        $offer->sms_status = 'Failed';
-                        $offer->save();
-                    }
-                    DB::commit();
-                } catch (\Exception $e) {
-                    Log::info('OFFER SEND ERROR: ' . $e->getMessage());
-                    DB::rollback();
-                }
-            }
+//            $user = env('SSL_USER_ID','ansarapi');
+//            $pass = env('SSL_PASSWORD','x83A7Z96');
+//            $sid = env('SSL_SID','ANSARVDP');
+//            $url = "http://sms.sslwireless.com/pushapi/dynamic/server.php";
+//            $offered_ansar = OfferSMS::where('sms_try', 0)->where('sms_status', 'Queue')->take(10)->get();
+//            foreach ($offered_ansar as $offer) {
+//                DB::beginTransaction();
+//                try {
+//                    $a = $offer->ansar;
+////                    Log::info($a);
+////                    break;
+//                    $dis = $offer->district->unit_name_eng;
+//                    $body = 'You (ID:' . $offer->ansar_id . ') are offered for ' . $dis . ' as Rank ' . $a->designation->name_eng . ' Please type (ans YES/ans NO) and send to 6969 within 48 hours. Otherwise your offer will be cancelled - DC ' . strtoupper($dis);
+//                    $phone = '88' . trim($a->mobile_no_self);
+//                    $param = "user=$user&pass=$pass&sms[0][0]=$phone&sms[0][1]=" . urlencode($body) . "&sid=$sid";
+//                    $crl = curl_init();
+//                    curl_setopt($crl, CURLOPT_SSL_VERIFYPEER, FALSE);
+//                    curl_setopt($crl, CURLOPT_SSL_VERIFYHOST, 2);
+//                    curl_setopt($crl, CURLOPT_URL, $url);
+//                    curl_setopt($crl, CURLOPT_HEADER, 0);
+//                    curl_setopt($crl, CURLOPT_RETURNTRANSFER, 1);
+//                    curl_setopt($crl, CURLOPT_POST, 1);
+//                    curl_setopt($crl, CURLOPT_POSTFIELDS, $param);
+//                    $response = curl_exec($crl);
+//                    curl_close($crl);
+//                    $r = Parser::xml($response);
+//                    Log::info(json_encode($r));
+//                    $offer->sms_try += 1;
+//                    if (isset($r['PARAMETER']) && strcasecmp($r['PARAMETER'], 'OK') == 0) {
+//                        $offer->sms_status = 'Send';
+//                        $offer->save();
+//                    } else {
+//                        $offer->sms_status = 'Failed';
+//                        $offer->save();
+//                    }
+//                    DB::commit();
+//                } catch (\Exception $e) {
+//                    Log::info('OFFER SEND ERROR: ' . $e->getMessage());
+//                    DB::rollback();
+//                }
+//            }
 
         })->everyMinute()->name("send_offer")->withoutOverlapping();
 //        $schedule->call(function () {
