e444c9063f81db6e792fbf0d8903f3f2bf75e0b6
arafat <shuvo1110101010@gmail.com>
2017-01-12 12:02:28 +0600

    update

--- local/app/Console/Kernel.php
+++ local/app/Console/Kernel.php
@@ -216,6 +216,7 @@ class Kernel extends ConsoleKernel
         })->dailyAt("00:00")->name('withdraw_kpi')->withoutOverlapping();
         $schedule->call(function () {
             $rest_ansars = RestInfoModel::whereDate('active_date','<=',Carbon::today()->toDateString())->whereIn('disembodiment_reason_id',[1,2,8])->get();
+            Log::info("REST to PANEl : CALLED");
             foreach($rest_ansars as $ansar){
                 DB::beginTransaction();
                 if(!in_array(AnsarStatusInfo::REST_STATUS,$ansar->status->getStatus())||in_array(AnsarStatusInfo::BLOCK_STATUS,$ansar->status->getStatus())||in_array(AnsarStatusInfo::BLACK_STATUS,$ansar->status->getStatus())) continue;
