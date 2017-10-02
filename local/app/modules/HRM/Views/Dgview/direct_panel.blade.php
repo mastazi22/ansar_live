6cf105c6cc2ff8da9b911696a428e08f0a3b6df4
fear1101010 <shuvo11101010@gmail.com>
2017-10-03 02:42:33 +0600

    embodiment

--- local/app/modules/HRM/Views/Dgview/direct_panel.blade.php
+++ local/app/modules/HRM/Views/Dgview/direct_panel.blade.php
@@ -17,7 +17,7 @@
             $scope.ansarId = "";
             $scope.ansarDetail = {};
             $scope.ansar_ids = [];
-            $scope.totalLength =  0;
+            $scope.totalLength = 0;
             $scope.loadingAnsar = false;
 
             $scope.loadAnsarDetail = function (id) {
@@ -35,14 +35,14 @@
             }
             $scope.makeQueue = function (id) {
                 $scope.ansar_ids.push(id);
-                $scope.totalLength +=  1;
+                $scope.totalLength += 1;
             }
-            $scope.$watch('totalLength', function (n,o) {
-                if(!$scope.loadingAnsar&&n>0){
+            $scope.$watch('totalLength', function (n, o) {
+                if (!$scope.loadingAnsar && n > 0) {
                     $scope.loadAnsarDetail($scope.ansar_ids.shift())
                 }
-                else{
-                    if(!$scope.ansarId)$scope.ansarDetail={}
+                else {
+                    if (!$scope.ansarId) $scope.ansarDetail = {}
                 }
             })
         })
@@ -57,102 +57,106 @@
                 </div>
             </div>
         @endif
-            @if(Session::has('error_message'))
-                <div style="padding: 10px 20px 0 20px;">
-                    <div class="alert alert-danger">
-                        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                        <span class="fa fa-remove"></span> {{Session::get('error_message')}}
-                    </div>
+        @if(Session::has('error_message'))
+            <div style="padding: 10px 20px 0 20px;">
+                <div class="alert alert-danger">
+                    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                    <span class="fa fa-remove"></span> {{Session::get('error_message')}}
                 </div>
-            @endif
+            </div>
+        @endif
         <section class="content" style="position: relative;">
             <div class="box box-solid">
-               <div class="box-body">
+                <div class="box-body">
 
-                   <div class="row">
-                       <div class="col-sm-4">
-                           {!! Form::model(Request::old(),array('route' => 'direct_panel_entry', 'id' => 'direct_panel_entry')) !!}
-                           <div class="form-group">
-                               {!! Form::label('ansar_id','Ansar ID to add to Panel') !!}
-                               {!! Form::text('ansar_id',null,['class'=>'form-control','placeholder'=>'Enter Ansar ID','ng-model'=>'ansarId','ng-init'=>"ansarId=".Request::old('ansar_id'),'ng-change'=>'makeQueue(ansarId)']) !!}
-                               {!! $errors->first('ansar_id','<p class="text text-danger">:message</p>') !!}
-                           </div>
-                           <div class="form-group">
-                               {!! Form::label('memorandum_id','Memorandum no.') !!}
-                               {!! Form::text('memorandum_id',null,['class'=>'form-control','placeholder'=>'Enter Memorandum no.']) !!}
-                               {!! $errors->first('memorandum_id','<p class="text text-danger">:message</p>') !!}
-                           </div>
-                           <div class="form-group">
-                               {!! Form::label('direct_panel_date','Panel Date') !!}
-                               {!! Form::text('direct_panel_date',null,['class'=>'form-control','id'=>'direct_panel_date','placeholder'=>'Enter Panel Date']) !!}
-                               {!! $errors->first('direct_panel_date','<p class="text text-danger">:message</p>') !!}
-                           </div>
-                           <div class="form-group">
-                               {!! Form::label('direct_panel_comment','Comment for adding to Panel') !!}
-                               {!! Form::textarea('direct_panel_comment', null, $attributes = array('class' => 'form-control', 'id' => 'direct_panel_comment', 'size' => '30x4', 'placeholder' => "Write Comment", 'ng-model' => 'direct_panel_comment')) !!}
-                           </div>
-                           <button id="add-panel-for-dg" class="btn btn-primary">
-                               <img ng-show="loadingSubmit" src="{{asset('dist/img/facebook-white.gif')}}"
-                                       width="16" style="margin-top: -2px">Add to Panel
-                           </button>
-                           {!! Form::close() !!}
-                       </div>
-                       <div class="col-sm-6 col-sm-offset-2"
-                            style="min-height: 400px;border-left: 1px solid #CCCCCC">
-                           <div id="loading-box" ng-if="loadingAnsar">
-                           </div>
-                           <div ng-if="ansarDetail.ansar_details.ansar_name_eng==undefined">
-                               <h3 style="text-align: center">No Ansar Found</h3>
-                           </div>
-                           <div ng-if="ansarDetail.ansar_details.ansar_name_eng!=undefined">
-                               <div class="form-group">
-                                   <label class="control-label">Name</label>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            {!! Form::model(Request::old(),array('route' => 'direct_panel_entry', 'id' => 'direct_panel_entry')) !!}
+                            <div class="form-group">
+                                {!! Form::label('ansar_id','Ansar ID to add to Panel') !!}
+                                @if(Request::old('ansar_id'))
+                                    {!! Form::text('ansar_id',null,['class'=>'form-control','placeholder'=>'Enter Ansar ID','ng-model'=>'ansarId','ng-init'=>"ansarId=".Request::old('ansar_id'),'ng-change'=>'makeQueue(ansarId)']) !!}
+                                @else
+                                    {!! Form::text('ansar_id',null,['class'=>'form-control','placeholder'=>'Enter Ansar ID','ng-model'=>'ansarId','ng-change'=>'makeQueue(ansarId)']) !!}
+                                @endif
+                                {!! $errors->first('ansar_id','<p class="text text-danger">:message</p>') !!}
+                            </div>
+                            <div class="form-group">
+                                {!! Form::label('memorandum_id','Memorandum no.') !!}
+                                {!! Form::text('memorandum_id',null,['class'=>'form-control','placeholder'=>'Enter Memorandum no.']) !!}
+                                {!! $errors->first('memorandum_id','<p class="text text-danger">:message</p>') !!}
+                            </div>
+                            <div class="form-group">
+                                {!! Form::label('direct_panel_date','Panel Date') !!}
+                                {!! Form::text('direct_panel_date',null,['class'=>'form-control','id'=>'direct_panel_date','placeholder'=>'Enter Panel Date']) !!}
+                                {!! $errors->first('direct_panel_date','<p class="text text-danger">:message</p>') !!}
+                            </div>
+                            <div class="form-group">
+                                {!! Form::label('direct_panel_comment','Comment for adding to Panel') !!}
+                                {!! Form::textarea('direct_panel_comment', null, $attributes = array('class' => 'form-control', 'id' => 'direct_panel_comment', 'size' => '30x4', 'placeholder' => "Write Comment", 'ng-model' => 'direct_panel_comment')) !!}
+                            </div>
+                            <button id="add-panel-for-dg" class="btn btn-primary">
+                                <img ng-show="loadingSubmit" src="{{asset('dist/img/facebook-white.gif')}}"
+                                     width="16" style="margin-top: -2px">Add to Panel
+                            </button>
+                            {!! Form::close() !!}
+                        </div>
+                        <div class="col-sm-6 col-sm-offset-2"
+                             style="min-height: 400px;border-left: 1px solid #CCCCCC">
+                            <div id="loading-box" ng-if="loadingAnsar">
+                            </div>
+                            <div ng-if="ansarDetail.ansar_details.ansar_name_eng==undefined">
+                                <h3 style="text-align: center">No Ansar Found</h3>
+                            </div>
+                            <div ng-if="ansarDetail.ansar_details.ansar_name_eng!=undefined">
+                                <div class="form-group">
+                                    <label class="control-label">Name</label>
 
-                                   <p>
-                                       [[ansarDetail.ansar_details.ansar_name_eng]]
-                                   </p>
-                               </div>
-                               <div class="form-group">
-                                   <label class="control-label">Rank</label>
+                                    <p>
+                                        [[ansarDetail.ansar_details.ansar_name_eng]]
+                                    </p>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label">Rank</label>
 
-                                   <p>
-                                       [[ansarDetail.ansar_details.name_eng]]
-                                   </p>
-                               </div>
-                               <div class="form-group">
-                                   <label class="control-label">Unit</label>
+                                    <p>
+                                        [[ansarDetail.ansar_details.name_eng]]
+                                    </p>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label">Unit</label>
 
-                                   <p>
-                                       [[ansarDetail.ansar_details.unit_name_eng]]
-                                   </p>
-                               </div>
-                               <div class="form-group">
-                                   <label class="control-label">Sex</label>
+                                    <p>
+                                        [[ansarDetail.ansar_details.unit_name_eng]]
+                                    </p>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label">Sex</label>
 
-                                   <p>
-                                       [[ansarDetail.ansar_details.sex]]
-                                   </p>
-                               </div>
-                               <div class="form-group">
-                                   <label class="control-label">Date of Birth</label>
+                                    <p>
+                                        [[ansarDetail.ansar_details.sex]]
+                                    </p>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label">Date of Birth</label>
 
-                                   <p>
-                                       [[ansarDetail.ansar_details.data_of_birth]]
-                                   </p>
-                               </div>
-                               <div class="form-group">
-                                   <label class="control-label status-check">Current Status</label>
+                                    <p>
+                                        [[ansarDetail.ansar_details.data_of_birth]]
+                                    </p>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label status-check">Current Status</label>
 
-                                   <p>
-                                       [[ansarDetail.status]]
-                                   </p>
-                               </div>
-                               <input type="hidden" name="ansar_status" value="[[ansarDetail.status]]">
-                           </div>
-                       </div>
-                   </div>
+                                    <p>
+                                        [[ansarDetail.status]]
+                                    </p>
+                                </div>
+                                <input type="hidden" name="ansar_status" value="[[ansarDetail.status]]">
+                            </div>
+                        </div>
+                    </div>
 
-               </div>
+                </div>
             </div>
         </section>
     </div>
