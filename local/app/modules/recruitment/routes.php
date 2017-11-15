b86c8d1e7e6713456ad6fdb5a4967b614379f6fb
arafat <arafat@shurjomukhi.com.bd>
2017-11-15 17:11:40 +0600

    login screen

--- local/app/modules/recruitment/routes.php
+++ local/app/modules/recruitment/routes.php
@@ -61,6 +61,7 @@ Route::group(['prefix'=>'recruitment','middleware'=>['auth','manageDatabase','ch
        $data = \Maatwebsite\Excel\Facades\Excel::load(storage_path('vulll.xls'),function (){
 
        })->get() ;
+       return $data;
        foreach ($data as $d){
            $jj = \App\modules\recruitment\Models\JobApplicantMarks::where('applicant_id',$d['applicant_id']);
            if(!$jj->exists()){
