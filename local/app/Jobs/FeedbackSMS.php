25d170373050bc43ef35e83ea6ff3ddb4ae01d8f
arafat <arafat@shurjomukhi.com.bd>
2018-03-01 12:11:33 +0600

    sms

--- local/app/Jobs/FeedbackSMS.php
+++ local/app/Jobs/FeedbackSMS.php
@@ -3,9 +3,11 @@
 namespace App\Jobs;
 
 use App\Jobs\Job;
+use App\modules\recruitment\Models\SmsQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\DB;
 
 class FeedbackSMS extends Job implements ShouldQueue
 {
@@ -31,8 +33,26 @@ class FeedbackSMS extends Job implements ShouldQueue
      */
     public function handle()
     {
+        DB::connection('recruitment')->beginTransaction();
+        try{
+            $payload = json_encode([
+                'to'=>$this->mobile,
+                'body'=>$this->message
+            ]);
+            SmsQueue::create([
+                'payload'=>$payload,
+                'try'=>0
+            ]);
+            DB::connection('recruitment')->commit();
+        }catch(\Exception $e){
+            DB::connection('recruitment')->rollback();
+        }catch(\Error $e){
+            DB::connection('recruitment')->rollback();
+        }catch(\Throwable $e){
+            DB::connection('recruitment')->rollback();
+        }
         //
-        $messID        = rand(1000,9999);
+        /*$messID        = rand(1000,9999);
         $messageID     = $messID;
         $apiUser       = 'join_ans_vdp';
         $apiPass       = 'shurjoSM123';
@@ -57,6 +77,6 @@ class FeedbackSMS extends Job implements ShouldQueue
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         $response = curl_exec($ch);
         curl_close ($ch);
-        var_dump ($response);
+        var_dump ($response);*/
     }
 }
