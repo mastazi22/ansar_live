1c08856a39d42e79ee488a0e89576bb5ee3c4634
arafat <arafat@shurjomukhi.com.bd>
2018-04-25 16:48:52 +0600

    avurp form validation

--- local/app/modules/AVURP/Controllers/ApiController.php
+++ local/app/modules/AVURP/Controllers/ApiController.php
@@ -44,6 +44,7 @@ class ApiController extends Controller
 
     public function store(VDPInfoRequest $request)
     {
+        return $request->all();
         $response = $this->infoRepository->create($request,$request->action_user_id);
         return response()->json($response);
 
