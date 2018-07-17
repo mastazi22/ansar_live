3a2862f7a608dcd16a6dc2646460a02f4b01d410
arafat <arafat@shurjomukhi.com.bd>
2018-07-17 18:23:49 +0600

    avurp

--- local/app/modules/recruitment/routes.php
+++ local/app/modules/recruitment/routes.php
@@ -85,9 +85,9 @@ Route::group(['prefix' => 'recruitment', 'middleware' => ['recruitment'], 'names
                 $p = unserialize($ee->previous_data);
 
                 $a = \App\modules\recruitment\Models\JobAppliciant::find($p["id"]);
-                $a->training_info = $p["training_info"];
-                $a->save();
-                array_push($z,$a->training_info." ".$p["applicant_id"]);
+                /*$a->training_info = $p["training_info"];
+                $a->save();*/
+                array_push($z,$a->training_info == $p["training_info"]);
             }
             return $z;
         });
