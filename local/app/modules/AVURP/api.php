ac583424c4eae655fa218d102844cba8fc2d78e2
arafat <arafat@shurjomukhi.com.bd>
2018-07-19 12:25:39 +0600

    avurp

--- local/app/modules/AVURP/api.php
+++ local/app/modules/AVURP/api.php
@@ -6,7 +6,7 @@ Route::group(['prefix' => 'AVURP/api', 'namespace' => '\App\modules\AVURP\Contro
     Route::get('info/image/{id}', ['as' => 'AVURP.api.image', 'uses' => 'ApiController@image']);
     Route::get('info/edit/{id}', ['as' => 'AVURP.api.edit', 'uses' => 'ApiController@edit']);
     Route::post('info/store', ['as' => 'AVURP.api.store', 'uses' => 'ApiController@store']);
-    Route::post('info/update/{id}', ['as' => 'AVURP.api.update', 'uses' => 'ApiController@update']);
+    Route::patch('info/update/{id}', ['as' => 'AVURP.api.update', 'uses' => 'ApiController@update']);
     Route::post('info/verify/{id}', ['as' => 'AVURP.info.verify', 'uses' => 'ApiController@verifyVDP']);
     Route::post('info/approve/{id}', ['as' => 'AVURP.info.approve', 'uses' => 'ApiController@approveVDP']);
     Route::post('info/verify_approve/{id}', ['as' => 'AVURP.info.verify_approve', 'uses' => 'ApiController@verifyAndApproveVDP']);
