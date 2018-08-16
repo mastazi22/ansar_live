2411f9761d6fed5385ebd6310bb8146f90909f24
arafat <arafat@shurjomukhi.com.bd>
2018-08-16 16:49:10 +0600

    avurp

--- local/app/Helper/EmailHelper.php
+++ local/app/Helper/EmailHelper.php
@@ -14,9 +14,20 @@ use Illuminate\Support\Facades\Mail;
 
 trait EmailHelper
 {
-        public function sendEmail($view,$data,$to,$subject="",$attachment=null){
-            return Mail::send($view,$data,function($message) use($to,$subject,$attachment){
+        public function sendEmail($view,$data,$to,$cc=null,$subject="",$attachment=null){
+            return Mail::send($view,$data,function($message) use($to,$subject,$attachment,$cc){
                 $message->to($to);
+                if($cc){
+                    if(is_array($cc)){
+                        foreach ($cc as $c){
+                            if(filter_var($c,FILTER_VALIDATE_EMAIL)){
+                                $message->cc($c);
+                            }
+                        }
+                    } elseif (filter_var($cc,FILTER_VALIDATE_EMAIL)){
+                        $message->cc($cc);
+                    }
+                }
                 $message->subject($subject);
                 if($attachment&&File::exists($attachment)){
                     $message->attach($attachment);
