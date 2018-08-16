b4db3ee81fb8d4c1153c4f5c43dfe0c521cde7f1
arafat <arafat@shurjomukhi.com.bd>
2018-08-16 18:08:01 +0600

    avurp

--- local/config/mail.php
+++ local/config/mail.php
@@ -71,7 +71,7 @@ return [
     */
 
 //    'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-    'encryption' => "",
+    'encryption' => "ssl",
 
     /*
     |--------------------------------------------------------------------------
