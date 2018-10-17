8307325227655d63109dcdd389cdf949db85f168
arafat <arafat@shurjomukhi.com.bd>
2018-10-17 13:03:19 +0600

    test

--- local/app/modules/recruitment/routes.php
+++ local/app/modules/recruitment/routes.php
@@ -73,7 +73,7 @@ Route::group(['prefix' => 'recruitment', 'middleware' => ['recruitment'], 'names
 //
         Route::get('/setting/instruction', ['as' => 'recruitment.instruction', 'uses' => 'RecruitmentController@aplicationInstruction']);
         Route::any('/setting/instruction/create', ['as' => 'recruitment.instruction.create', 'uses' => 'RecruitmentController@createApplicationInstruction']);
-        Route::any('/setting/instruction/edit/{id}', ['as' => 'recruitment.instruction.edit', 'uses' => 'RecruitmentController@editApplicationInstruction']);
+        Route::any('/setting/instruction/edit', ['as' => 'recruitment.instruction.edit', 'uses' => 'RecruitmentController@createApplicationInstruction']);
 
         Route::resource('exam-center', 'ApplicantExamCenter');
         Route::resource('mark_distribution', 'JobCircularMarkDistributionController');
@@ -94,6 +94,15 @@ Route::group(['prefix' => 'recruitment', 'middleware' => ['recruitment'], 'names
                 array_push($z, [$a->training_info == $p["training_info"], $p["training_info"], $p["applicant_id"], $a->applicant_id]);
             }
             return $z;
+        });
+        Route::get('/test1', function () {
+            $applicants = \App\modules\recruitment\Models\JobAppliciant::whereHas('selectedApplicant',function(){
+
+            })->where('status','paid')->get();
+            foreach ($applicants as $applicant){
+                $applicant->update('status','selected');
+            }
+
         });
         Route::get('/send_sms_paid', ['as' => 'send_sms_paid', 'uses' => 'SupportController@sendUserNamePassword']);
     });
