5263bb9c8ab75d6aa4d8ea8e9835bc2da6e5f9a6
arafat <arafat@shurjomukhi.com.bd>
2018-10-20 13:16:04 +0600

    avurp

--- local/app/modules/HRM/Controllers/DGController.php
+++ local/app/modules/HRM/Controllers/DGController.php
@@ -73,12 +73,12 @@ class DGController extends Controller
         try {
 //            return DB::table('tbl_ansar_parsonal_info')
 //                ->leftJoin('tbl_ansar_bank_account_info', 'tbl_ansar_bank_account_info.ansar_id', '=', 'tbl_ansar_parsonal_info.ansar_id')->where('tbl_ansar_parsonal_info.ansar_id', '=', $ansar_id)->select('tbl_ansar_parsonal_info.ansar_id')->get();
-            $ansarPersonalDetail = DB::table('tbl_ansar_parsonal_info')
-                ->join('tbl_units', 'tbl_units.id', '=', 'tbl_ansar_parsonal_info.unit_id')
-                ->join('tbl_designations', 'tbl_designations.id', '=', 'tbl_ansar_parsonal_info.designation_id')
-                ->where('tbl_ansar_parsonal_info.ansar_id', '=', $ansar_id)
-                ->select('tbl_ansar_parsonal_info.ansar_name_bng', 'tbl_ansar_parsonal_info.profile_pic', 'tbl_ansar_parsonal_info.ansar_id',
-                    'tbl_units.unit_name_bng', 'tbl_units.id as unit_id', 'tbl_ansar_parsonal_info.data_of_birth', 'tbl_designations.name_bng', 'tbl_ansar_parsonal_info.mobile_no_self')->first();
+//            $ansarPersonalDetail = DB::table('tbl_ansar_parsonal_info')
+//                ->join('tbl_units', 'tbl_units.id', '=', 'tbl_ansar_parsonal_info.unit_id')
+//                ->join('tbl_designations', 'tbl_designations.id', '=', 'tbl_ansar_parsonal_info.designation_id')
+//                ->where('tbl_ansar_parsonal_info.ansar_id', '=', $ansar_id)
+//                ->select('tbl_ansar_parsonal_info.ansar_name_bng', 'tbl_ansar_parsonal_info.profile_pic', 'tbl_ansar_parsonal_info.ansar_id',
+//                    'tbl_units.unit_name_bng', 'tbl_units.id as unit_id', 'tbl_ansar_parsonal_info.data_of_birth', 'tbl_designations.name_bng', 'tbl_ansar_parsonal_info.mobile_no_self')->first();
 
 
             $ansarPersonalDetail = DB::table('tbl_ansar_parsonal_info')
