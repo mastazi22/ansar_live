260be582ba9a4ca979bc7a142c056d4e837ff129
arafat <arafat@shurjomukhi.com.bd>
2019-06-18 11:09:45 +0600

    block for age added

--- local/app/Console/Kernel.php
+++ local/app/Console/Kernel.php
@@ -7,6 +7,7 @@ use App\Helper\Facades\GlobalParameterFacades;
 use App\Helper\Helper;
 use App\Helper\GlobalParameter;
 use App\Helper\SMSTrait;
+use App\Jobs\BlockForAge;
 use App\modules\HRM\Models\AnsarRetireHistory;
 use App\modules\HRM\Models\AnsarStatusInfo;
 use App\modules\HRM\Models\EmbodimentModel;
@@ -504,59 +505,13 @@ class Kernel extends ConsoleKernel
 //        })->everyThirtyMinutes()->name("offer_block_to_panel_6_month")->withoutOverlapping();
         $schedule->call(function () {
             Log::info("called : Ansar Block For Age");
-            $ansars = PanelModel::whereHas('ansarInfo.status',function ($q){
+            $count = PanelModel::whereHas('ansarInfo.status',function ($q){
                 $q->where('block_list_status',0);
                 $q->where('pannel_status',1);
                 $q->where('black_list_status',0);
-            })->with(['ansarInfo'=>function($q){
-                $q->select('ansar_id','data_of_birth','designation_id');
-                $q->with(['designation','status']);
-            }])->take(500)->get();
-//            return $ansars;
-            $a = [];
-            DB::connection('hrm')->beginTransaction();
-            try {
-                $now = \Carbon\Carbon::now();
-                foreach ($ansars as $ansar) {
-
-                    $info = $ansar->ansarInfo;
-                    $dob = $info->data_of_birth;
-
-                    $age = \Carbon\Carbon::parse($dob)->diff($now, true);
-                    $ansarRe = GlobalParameterFacades::getValue('retirement_age_ansar') - 3;
-                    $pcApcRe = GlobalParameterFacades::getValue('retirement_age_pc_apc') - 3;
-                    Log::info("called : Ansar Block For Age-".$ansar->ansar_id."Age:".$age->y."year ".$age->m."month ".$age->d." days");
-                    if ($info->designation->code == "ANSAR" && ($age->y >= $ansarRe&&($age->m>0||$age->d>0))) {
-                        $info->status->update([
-                            'pannel_status' => 0,
-                            'retierment_status' => 1
-                        ]);
-                        $info->retireHistory()->create([
-                            'retire_from'=>'panel',
-                            'retire_date'=>$now->format('Y-m-d')
-                        ]);
-                        $ansar->saveLog('Retire', null, 'over aged');
-                        $ansar->delete();
-                    } else if (($info->designation->code == "PC" || $info->designation->code == "APC") && ($age->y >= $pcApcRe&&($age->m>0||$age->d>0))) {
-                        $info->status->update([
-                            'pannel_status' => 0,
-                            'retierment_status' => 1
-                        ]);
-                        $info->retireHistory()->create([
-                            'retire_from'=>'panel',
-                            'retire_date'=>$now->format('Y-m-d')
-                        ]);
-                        $ansar->saveLog('Retire', null, 'over aged');
-                        $ansar->delete();
-                    }
-
-                    //array_push($a, ['ansar_id' => $ansar->ansar_id, 'age' => $age, 'status' => $info->status->getStatus()]);
-
-                }
-                DB::connection('hrm')->commit();
-            }catch(\Exception $e){
-                Log::info("ansar_block_for_age:".$e->getMessage());
-                DB::connection('hrm')->rollback();
+            })->count();
+            for($i=0;$i<$count;$i+=500){
+                dispatch(new BlockForAge($i));
             }
 
 
