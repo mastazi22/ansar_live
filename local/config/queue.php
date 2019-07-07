3b75658eddc1866b8a53984af23510f64f885bf0
arafat <arafat@shurjomukhi.com.bd>
2019-07-07 14:16:51 +0600

    panel position v1

--- local/config/queue.php
+++ local/config/queue.php
@@ -39,7 +39,7 @@ return [
             'driver' => 'database',
             'table' => 'jobs',
             'queue' => 'default',
-            'expire' => 600,
+            'expire' => 600000000,
         ],
         'recruitment' => [
             'connection' => 'recruitment',
