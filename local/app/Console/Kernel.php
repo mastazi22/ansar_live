a42ccbed5cc0fc398deac2883d723ae260c7ddb6
arafat <shuvo11101010@gmail.com>
2019-08-01 00:19:42 +0600

    UnblockRetireAnsar

--- local/app/Console/Kernel.php
+++ local/app/Console/Kernel.php
@@ -533,10 +533,57 @@ class Kernel extends ConsoleKernel
         })->everyMinute()->name("ansar_block_for_age3")->withoutOverlapping();
         $schedule->call(function () {
 
-            dispatch(new UnblockRetireAnsar());
+            $ansars = AnsarRetireHistory::all();
+            DB::connection('hrm')->beginTransaction();
+            try {
+                $now = \Carbon\Carbon::now();
+                foreach ($ansars as $ansar) {
+
+                    $info = $ansar->ansar;
+                    $dob = $info->data_of_birth;
+
+                    $age = \Carbon\Carbon::parse($dob)->diff($now, true);
+                    $ansarRe = GlobalParameterFacades::getValue('retirement_age_ansar') - 3;
+                    $pcApcRe = GlobalParameterFacades::getValue('retirement_age_pc_apc') - 3;
+                    //echo("called : Ansar Block For Age-".$ansar->ansar_id."Age:".$age->y."year ".$age->m."month ".$age->d." days");
+                    if ($info->designation->code == "ANSAR" && $age->y < $ansarRe) {
+                        $pl = PanelInfoLogModel::where('ansar_id',$info->ansar_id)->orderBy('panel_date','desc')->first();
+                        $info->panel()->create([
+                            'ansar_merit_list'=>$pl->merit_list,
+                            'panel_date'=>Carbon::now()->format('Y-m-d'),
+                            'memorandum_id'=>$pl->old_memorandum_id,
+                            'come_from'=>'After Retier'
+                        ]);
+                        $info->status->update([
+                            'pannel_status' => 1,
+                            'retierment_status' => 0
+                        ]);
+                        $ansar->delete();
+                    } else if (($info->designation->code == "PC" || $info->designation->code == "APC") && $age->y < $pcApcRe) {
+                        $info->panel()->create([
+                            'ansar_merit_list'=>$pl->merit_list,
+                            'panel_date'=>Carbon::now()->format('Y-m-d'),
+                            'memorandum_id'=>$pl->old_memorandum_id,
+                            'come_from'=>'After Retier'
+                        ]);
+                        $info->status->update([
+                            'pannel_status' => 1,
+                            'retierment_status' => 0
+                        ]);
+                        $ansar->delete();
+                    }
+                }
+
+                DB::connection('hrm')->commit();
+            }catch(\Exception $e){
+                Log::info("ansar_unblock_for_age:".$e->getMessage());
+                DB::connection('hrm')->rollback();
+            }
+            dispatch(new RearrangePanelPositionGlobal());
+            dispatch(new RearrangePanelPositionLocal());
 
 
-        })->dailyAt("00:00")->name("UnblockRetireAnsar")->withoutOverlapping();
+        })->everyTenMinutes()->name("UnblockRetireAnsar")->withoutOverlapping();
 
     }
 }
