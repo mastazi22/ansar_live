c511e738f4fd0051ac81d257ba40e8c63fc7af91
arafat <arafat@shurjomukhi.com.bd>
2019-09-07 00:02:28 +0600

    embodiment

--- local/app/modules/recruitment/routes.php
+++ local/app/modules/recruitment/routes.php
@@ -95,7 +95,7 @@ Route::group(['prefix' => 'recruitment', 'middleware' => ['recruitment'], 'names
                 ->join('job_applicant_exam_center','job_circular.id','=','job_applicant_exam_center.job_circular_id')
                 ->join('db_amis.tbl_division','db_amis.tbl_division.id','=','job_applicant.present_division_id')
                 ->where('job_applicant.status','selected')
-                ->whereIn('job_circular.id',[52])
+                ->whereIn('job_circular.id',[48])
                 ->select('job_applicant.applicant_id','job_applicant.applicant_name_bng','job_applicant.roll_no','job_circular.circular_name','job_applicant.applicant_password',
                     'division_name_bng','mobile_no_self','job_applicant_exam_center.selection_place','job_applicant_exam_center.exam_place_roll_wise',
                     'job_applicant_exam_center.selection_date','job_applicant_exam_center.selection_time')
