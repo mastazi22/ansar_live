c1e6ec7d875b1ab4aaed39d47b38a6c0e084bd78
DESKTOP-G0MFRGV\Admin <sabbirul@shurjomukhi.com.bd>
2019-11-05 19:11:03 +0600

    karnel file modified

--- local/app/Console/Kernel.php
+++ local/app/Console/Kernel.php
@@ -615,7 +615,7 @@ class Kernel extends ConsoleKernel
             }
 
 
-        })->everyMinute()->name("ansar_block_for_age3")->withoutOverlapping();
+        })->daily()->name("ansar_block_for_age3")->withoutOverlapping();
         $schedule->call(function () {
             Log::info("ansar_unblock_for_age:");
             $ansars = AnsarRetireHistory::all();
