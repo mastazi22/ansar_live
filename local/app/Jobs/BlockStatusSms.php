4cf8ab39ef12227e006eeb1e6c18ad8c25be9a58
Md. Sabbirul Latif <sabbirul@shurjomukhi.com.bd>
2020-03-23 10:21:03 +0600

    merge conflict

--- local/app/Jobs/BlockStatusSms.php
+++ local/app/Jobs/BlockStatusSms.php
@@ -21,7 +21,8 @@ class BlockStatusSms extends Job implements SelfHandling, ShouldQueue
      */
     protected $ansar_id;
     protected $reason;
-    public function __construct($ansar_id,$reason)
+
+    public function __construct($ansar_id, $reason)
     {
         $this->ansar_id = $ansar_id;
         $this->reason = $reason;
@@ -35,29 +36,37 @@ class BlockStatusSms extends Job implements SelfHandling, ShouldQueue
      */
     public function handle()
     {
-        //
-/*        $user = env('SSL_USER_ID');
+        $send_sms_env = env("SEND_SMS", false);
+        $test_mobile = env("TEST_SMS_NUMBER", null);
+        $phone_number = "";
+        $ansar = PersonalInfo::where('ansar_id', $this->ansar_id)->select('mobile_no_self')->first();
+        if ($send_sms_env == false && !empty($test_mobile) && $ansar) {
+            //Test SMS SEND config=
+            $phone_number = $test_mobile;
+        } elseif ($send_sms_env == true && $ansar) {
+            $phone_number = $ansar->mobile_no_self;
+        } else {
+            return null;
+        }
+        $user = env('SSL_USER_ID');
         $pass = env('SSL_PASSWORD');
         $sid = env('SSL_SID');
         $url = "http://sms.sslwireless.com/pushapi/dynamic/server.php";
-        $ansar = PersonalInfo::where('ansar_id',$this->ansar_id)->select('mobile_no_self')->first();
-        if($ansar){
-            Log::info("EMBODIED ".$this->ansar_id);
-            $phone = '88'.trim($ansar->mobile_no_self);
-            $body = "You are Blocked due to ".$this->reason;
-            $param = "user=$user&pass=$pass&sms[0][0]=$phone&sms[0][1]=" . urlencode($body) . "&sid=$sid";
-            $crl = curl_init();
-            curl_setopt($crl, CURLOPT_SSL_VERIFYPEER, FALSE);
-            curl_setopt($crl, CURLOPT_SSL_VERIFYHOST, 2);
-            curl_setopt($crl, CURLOPT_URL, $url);
-            curl_setopt($crl, CURLOPT_HEADER, 0);
-            curl_setopt($crl, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($crl, CURLOPT_POST, 1);
-            curl_setopt($crl, CURLOPT_POSTFIELDS, $param);
-            $response = curl_exec($crl);
-            curl_close($crl);
-            $r = Parser::xml($response);
-            Log::info($r);
-        }*/
+        Log::info("EMBODIED " . $this->ansar_id);
+        $phone = '88' . trim($phone_number);
+        $body = "You are Blocked due to " . $this->reason;
+        $param = "user=$user&pass=$pass&sms[0][0]=$phone&sms[0][1]=" . urlencode($body) . "&sid=$sid";
+        $crl = curl_init();
+        curl_setopt($crl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($crl, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($crl, CURLOPT_URL, $url);
+        curl_setopt($crl, CURLOPT_HEADER, 0);
+        curl_setopt($crl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($crl, CURLOPT_POST, 1);
+        curl_setopt($crl, CURLOPT_POSTFIELDS, $param);
+        $response = curl_exec($crl);
+        curl_close($crl);
+        $r = Parser::xml($response);
+        Log::info($r);
     }
 }
