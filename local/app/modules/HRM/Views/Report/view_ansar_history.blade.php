2e8f3b3ed2f1bef8a713d9f23728cbd02ccb0ad7
Md. Sabbirul Latif <sabbirul@shurjomukhi.com.bd>
2020-04-06 14:28:18 +0600

    update

--- local/app/modules/HRM/Views/Report/view_ansar_history.blade.php
+++ local/app/modules/HRM/Views/Report/view_ansar_history.blade.php
@@ -104,7 +104,8 @@
                                     </td>
                                     <td>[[ansarDetail['ansar'].mobile_no_self]]</td>
                                     <td>[[ansarDetail['ansar'].sex]]</td>
-                                    <td><img src="/image?file=[[ansarDetail['ansar'].profile_pic]]" style="width: 80px;height: 80px"
+                                    <td><img src="/image?file=[[ansarDetail['ansar'].profile_pic]]"
+                                             style="width: 80px;height: 80px"
                                              alt="[[ansarDetail['ansar'].ansar_name_bng]]"/></td>
                                 </tr>
                                 </tbody>
@@ -236,11 +237,15 @@
                                 </tr>
                                 </thead>
                                 <tbody>
-                                <tr>
+                                <tr ng-style="ansarDetail['cPanel'].locked==1?{'background': 'red','color':'white'}:''">
                                     <td>[[convertDateObj(ansarDetail['cPanel'].panel_date) | date:'medium']]</td>
-                                    <td>[[ansarDetail['cPanel'].go_panel_position==null?"Blocked":ansarDetail['cPanel'].go_panel_position]]</td>
+                                    <td ng-style="(ansarDetail['cPanel'].go_panel_position==null && ansarDetail['cPanel'].locked==0)? {'background': 'orange','color':'white'}:''">
+                                        [[ansarDetail['cPanel'].go_panel_position==null?"Offer Blocked":ansarDetail['cPanel'].go_panel_position]]
+                                    </td>
                                     <td>[[convertDateObj(ansarDetail['cPanel'].re_panel_date) | date:'medium']]</td>
-                                    <td>[[ansarDetail['cPanel'].re_panel_position==null?"Blocked":ansarDetail['cPanel'].re_panel_position]]</td>
+                                    <td ng-style="(ansarDetail['cPanel'].re_panel_position==null && ansarDetail['cPanel'].locked==0)? {'background': 'orange','color':'white'}:''">
+                                        [[ansarDetail['cPanel'].re_panel_position==null?"Offer Blocked":ansarDetail['cPanel'].re_panel_position]]
+                                    </td>
                                     <td>[[ansarDetail['cPanel'].come_from]]</td>
                                     <td>[[ansarDetail['cPanel'].memorandum_id]]</td>
                                     <td>[[ansarDetail['cPanel'].action_user_id]]</td>
